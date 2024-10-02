8607d5e3ad6a7477d3ee4756abb9117ba1b1f348
Alam Sanchez <asancheztorres1905@gmail.com>
2024-10-02 10:29:43 +0200

    Move to utest.h

--- assignment1/test/test_integration.cpp
+++ assignment1/test/test_integration.cpp
@@ -1,27 +1,25 @@
 #include "../src/integration.hpp"
-#include <gtest/gtest.h>
 #include <stdexcept>
+#include "utest.h"
 
-TEST(Integration, TestIntegrate) {
+UTEST(Integration, TestIntegrate) {
   auto f = [](double x) { return x; };
   double result = integrate(f, 0.0, 1.0, 1e-2);
 
-  EXPECT_NEAR(result, 0.5, 1e-2);
+  ASSERT_NEAR(result, 0.5, 1e-2);
 }
 
-TEST(Integration, TestNegativeTolerance) {
+UTEST(Integration, TestNegativeTolerance) {
   auto f = [](double x) { return x; };
 
-  EXPECT_THROW(integrate(f, 0.0, 1.0, -1e-2), std::domain_error);
+  ASSERT_EXCEPTION(integrate(f, 0.0, 1.0, -1e-2), std::domain_error);
 }
 
-TEST(Integration, TestZeroTolerance) {
+UTEST(Integration, TestZeroTolerance) {
   auto f = [](double x) { return x; };
 
-  EXPECT_THROW(integrate(f, 0.0, 1.0, 0.0), std::runtime_error);
+  ASSERT_EXCEPTION(integrate(f, 0.0, 1.0, -0.0), std::domain_error);
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+// Main function to run all tests
+UTEST_MAIN();
